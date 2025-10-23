ee68885ae729e808977aec6d109a35ea6326bb33
Hamidreza <hamidreza.tahmooresi@gmail.com>
2025-10-24 00:00:39 +0330

    add field to components

--- src/pages/miniapp/settings.tsx
+++ src/pages/miniapp/settings.tsx
@@ -1,4 +1,6 @@
 import {ModeToggle} from "@/components/mode-toggle.tsx";
+import { Field, FieldDescription, FieldError, FieldGroup, FieldLabel, FieldLegend, FieldSet } from "@/components/ui/field";
+import type { Input } from "@/components/ui/input";
 import UserMenu from "@/components/UserMenu.tsx";
 // import {GalleryVerticalEnd} from "lucide-react";
 
@@ -7,22 +9,23 @@ function Index() {
 
     return (
         <>
-            <div>
-                {/*<MobileLayout>*/}
-                <div>Settings</div>
+        <FieldSet>
+  <FieldLegend>Settings</FieldLegend>
+  
+  <FieldGroup>
+    <Field>
+      <FieldLabel htmlFor="name">Mode</FieldLabel>
+      <ModeToggle />
+      <FieldDescription>This appears on invoices and emails.</FieldDescription>
+    </Field>
+    <Field>
+      <FieldLabel htmlFor="username">Language</FieldLabel>
+      <UserMenu  />
+    </Field>
+    
+  </FieldGroup>
+</FieldSet>
 
-
-                <div>
-                    <label htmlFor="theme-toggle">Theme Selection</label>
-                    <ModeToggle />
-                </div>
-                <div>
-                    <label htmlFor="language-menu">Language Selection</label>
-                    <UserMenu  />
-                </div>
-                {/*</MobileLayout>*/}
-
-            </div>
         </>
     )
 }
